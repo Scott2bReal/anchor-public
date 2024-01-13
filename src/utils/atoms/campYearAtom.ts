e6863e5a175556e4513040d9c23ec2c6b8c0ecae
Scott Hoecker <hoeckers@gmail.com>
2024-01-12 21:20:08 -0600

    Use 2023 for init year

--- src/utils/atoms/campYearAtom.ts
+++ src/utils/atoms/campYearAtom.ts
@@ -3,4 +3,5 @@ import { atom } from "jotai";
 // Automatically sets the default selected campYear to the current year. Make
 // sure to check THIS atom vs. the current year elsewhere to maintain global
 // state
-export const campYearAtom = atom<number>(new Date().getFullYear())
+// export const campYearAtom = atom<number>(new Date().getFullYear())
+export const campYearAtom = atom<number>(2023)
